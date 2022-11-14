8b640e55a41390ee6c18354675c1622a0eef5280
Joshua Ramat <ramatjoshua@gmail.com>
2022-11-14 11:17:26 -0800

    set up app to begin adding components and pages, finished sidebar component

---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Kanban } from './Kanban';
+export { default as Employees } from './Employees';
+export { default as ColorPicker } from './ColorPicker';
+export { default as Calendar } from './Calendar';
+export {default as Home } from './Home';
