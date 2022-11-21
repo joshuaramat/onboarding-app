894841b188140f277babbcb6ed893f050854ba77
Joshua Ramat <ramatjoshua@gmail.com>
2022-11-21 13:46:43 -0800

    ADD Navbar component

--- src/App.js
+++ src/App.js
@@ -37,10 +37,13 @@ const App = () => {
               <Sidebar />
             </div>
           )}
-          <div className={
-            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
-          }>
-            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+          <div 
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}
+          >
+            <div 
+              className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'
+            >
+              <Navbar />
             </div>
           </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import avatar from '../data/avatar.jpg';
+import { Notification, UserProfile } from '.';
+
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent 
+    content={title}
+    position="BottomCenter"
+  >
+    <button
+      type="button"
+      onClick={customFunc}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      >
+        {icon}
+      </span>
+    </button>
+  </TooltipComponent>
+)
+
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Navbar = () => {
+  const { activeMenu, setActiveMenu } = useStateContext();
+
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between p-2 md:mx-6 relative'>
+      <NavButton 
+        title="Menu" 
+        customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} 
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+
+      <div className='flex'>
+        <NavButton 
+          title="Notifications"
+          dotColor="#03C9D7" 
+          customFunc={() => handleClick('notification')} 
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent
+          content="Profile"
+          position="BottomCenter"
+        >
+          <div
+            className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-1g'
+            onClick={() => handleClick('userProfile')}
+          >
+            <img
+              className='rounded-full w-8 h-8'
+              src={avatar}
+            />
+            <p>
+              <span className='text-gray-400 text-14'>Hi, </span> {' '}
+              <span text-gray-400 ml-1 text-14>Michael</span>
+            </p>
+            <MdKeyboardArrowDown
+              className='text-gray-400 text-14'
+            />
+          </div>
+        </TooltipComponent>
+      </div>
+    </div>
   )
 }
 
